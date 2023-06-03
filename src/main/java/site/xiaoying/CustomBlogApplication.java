bb0b4950534a5757cb0ecebe71f1d5202d1b22b3
挚爱小v脸 <3047893225@qq.com>
2023-06-03 23:42:05 +0800

    初始化

---
+++ src/main/java/site/xiaoying/CustomBlogApplication.java
@@ -0,0 +1,18 @@
+package site.xiaoying;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * 自定义博客应用程序
+ *
+ * @author 小樱
+ * @date 2023/06/03
+ */
+@SpringBootApplication
+public class CustomBlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CustomBlogApplication.class,args);
+    }
+}
